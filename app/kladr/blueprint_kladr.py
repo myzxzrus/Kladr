f50ec9302b5cc4987b5f91a630046cdee1ad29a1
myzxzrus <myzxzrus@gmail.com>
2020-05-15 01:52:39 +0300

    15052020

--- app/kladr/blueprint_kladr.py
+++ app/kladr/blueprint_kladr.py
@@ -1,26 +1,18 @@
 from flask import Blueprint
 from flask import render_template
-from models_kladr import Kladr, Street
+from models_kladr import Kladr, Street, Doma, Altnames
 
 
 kladr = Blueprint('kladr', __name__, template_folder='templates')
 
-# def query_kladr():
-#     kladr_list = []
-#     for i in range(1, 100):
-#         st = '0'
-#
-#         if i < 10:
-#             st = '0' + str(i) + '00000000000'
-#         else:
-#             st = str(i) + '00000000000'
-#         p = Kladr.query.filter(Kladr.code.contains(st)).all()
-#         if not p == []:
-#             p1 = p[0]
-#             p2 = str(p1).split(':')
-#             p3 = '{} {} - {}'.format(p2[1], p2[3], str(i))
-#         kladr_list.append(p3)
-#     return kladr_list
+def altnames_kladr(code):
+    if code[-2:] == '51':
+        code = code[:-2] + '00'
+        address = Altnames.query.filter(Altnames.oldcode.like(code)).first()
+        return address.newcode
+    else:
+        return code
+
 
 
 @kladr.route('/')
@@ -32,18 +24,19 @@ def index():
 
 @kladr.route('/<region>')  # <region> имя параметра
 def kladr_region(region):
+    print('kladr_region')
     # for region
     like_district = '{}___000000__'.format(region)
     like_citi = '{}000___000__'.format(region)
     like_settlement = '{}000000_____'.format(region)
-    # 46 000 001 000 00 - Citi 4600000100000 1600 - street
+    # 46 000 001 000 00 - Citi 4600000100000 1600 - street СС РРР ГГГ ППП УУУУ АА
     like_street = '{}000000000______'.format(region)
     notquery = '{}00000000000'.format(region)
     address = Kladr.query.filter(Kladr.code.like(notquery)).all()
-    district = Kladr.query.filter(Kladr.code.like(like_district), Kladr.code != notquery).all()
-    citi = Kladr.query.filter(Kladr.code.like(like_citi), Kladr.code != notquery).all()
-    settlement = Kladr.query.filter(Kladr.code.like(like_settlement), Kladr.code != notquery).all()
-    street = Street.query.filter(Street.code.like(like_street)).all()
+    district = Kladr.query.filter(Kladr.code.like(like_district), Kladr.code != notquery).order_by(Kladr.name).all()
+    citi = Kladr.query.filter(Kladr.code.like(like_citi), Kladr.code != notquery).order_by(Kladr.name).all()
+    settlement = Kladr.query.filter(Kladr.code.like(like_settlement), Kladr.code != notquery).order_by(Kladr.name).all()
+    street = Street.query.filter(Street.code.like(like_street)).order_by(Street.name).all()
     template_context = dict(district=district, citi=citi, settlement=settlement, street=street, address=address, code=notquery)
     return render_template('kladr/kladr_region.html', **template_context)
 
@@ -51,12 +44,14 @@ def kladr_region(region):
 @kladr.route('/<region>/<subtotal_region>')  # <level> имя параметра
 def kladr_subtotal_region(region, subtotal_region):
     # for distrikt
+    print('kladr_subtotal_region')
     if len(subtotal_region) == 3:
         like_citi = '{}___000__'.format(region+subtotal_region)
         like_settlement = '{}000_____'.format(region+subtotal_region)
         notquery = '{}00000000'.format(region+subtotal_region)
-        citi = Kladr.query.filter(Kladr.code.like(like_citi), Kladr.code != notquery).all()
-        settlement = Kladr.query.filter(Kladr.code.like(like_settlement), Kladr.code != notquery).all()
+        citi = Kladr.query.filter(Kladr.code.like(like_citi), Kladr.code != notquery).order_by(Kladr.name).all()
+        settlement = Kladr.query.filter(Kladr.code.like(like_settlement), Kladr.code != notquery).order_by(Kladr.name).all()
+
         address = Kladr.query.filter(Kladr.code.like(notquery)).all()
         template_context = dict(chek='districkt', citi=citi, settlement=settlement, address=address, code=notquery)
     # for citi
@@ -65,21 +60,63 @@ def kladr_subtotal_region(region, subtotal_region):
         # 46 000 001 000 00 - Citi 4600000100000 1600 - street
         like_street = '{}______'.format(region + subtotal_region[:9])
         notquery = '{}'.format(region + subtotal_region)
-        settlement = Kladr.query.filter(Kladr.code.like(like_settlement), Kladr.code != notquery).all()
-        street = Street.query.filter(Street.code.like(like_street)).all()
+        settlement = Kladr.query.filter(Kladr.code.like(like_settlement), Kladr.code != notquery).order_by(Kladr.name).all()
+        street = Street.query.filter(Street.code.like(like_street)).order_by(Street.name).all()
+        altnames = altnames_kladr(notquery)
+        print(altnames)
         address = Kladr.query.filter(Kladr.code.like(notquery)).all()
-        template_context = dict(chek='citi', street=street, settlement=settlement, address=address, code=notquery)
+        print(address)
+        template_context = dict(chek='citi', street=street, settlement=settlement, address=address, code=notquery, altnames=altnames)
     # for settlement
     if len(subtotal_region) == 13:
         # 46 000 001 000 00 - Citi 4600000100000 1600 - street
         like_street = '{}______'.format(subtotal_region[:11])
-        street = Street.query.filter(Street.code.like(like_street)).all()
+        street = Street.query.filter(Street.code.like(like_street)).order_by(Street.name).all()
+        altnames = altnames_kladr(subtotal_region)
         address = Kladr.query.filter(Kladr.code.like(subtotal_region)).all()
-        template_context = dict(chek='settlement', street=street, address=address, code=subtotal_region)
+        print(altnames)
+        print(address)
+        template_context = dict(chek='settlement', street=street, address=address, code=subtotal_region, altnames=altnames)
     return render_template('kladr/kladr_subtotal_region.html', **template_context)
 
 
 @kladr.route('/<region>/<subtotal_region>/<street>')  # <level> имя параметра
 def kladr_street(region, subtotal_region, street):
-    print(region, subtotal_region, street)
-    # return render_template('kladr/kladr_subtotal_region.html', **template_context)
+    print('kladr_street')
+    # if len(subtotal_region) == 13:
+    # 46 00000100000 1600 - street 46 00000100000 1600 ____
+    like_doma = '{}__'.format(region + subtotal_region + street)
+    like_address = '{}'.format(region + subtotal_region + street)
+    doma = Doma.query.filter(Doma.code.like(like_doma)).order_by(Doma.name).all()
+    altnames = altnames_kladr(like_address)
+    address = Street.query.filter(Street.code.like(like_address)).all()
+    template_context = dict(chek='doma', doma=doma, address=address, altnames=altnames)
+    return render_template('kladr/kladr_street.html', **template_context)
+
+@kladr.route('/distrikt/<region>/<subtotal_region>/<cheek>')  # <level> имя параметра
+def kladr_distrikt(region, subtotal_region, cheek):
+    print('kladr_distrikt')
+    if int(subtotal_region[-2:]) >= 1 and int(subtotal_region[-2:]) <= 50:
+        subtotal_region = subtotal_region[:-2] + '00'
+    if cheek == 'c':
+        # region = 46; subtotal_region = 00400100000;
+        like_street = '{}____'.format(region + subtotal_region)
+        like_settlement = '{}_____'.format(region + subtotal_region[:6])
+        street = Street.query.filter(Street.code.like(like_street)).order_by(Street.name).all()
+        notquery = '{}'.format(region + subtotal_region)
+        settlement = Kladr.query.filter(Kladr.code.like(like_settlement), Kladr.code != notquery).order_by(Kladr.name).all()
+        altnames = altnames_kladr(notquery)
+        address = Kladr.query.filter(Kladr.code.like(notquery)).all()
+        template_context = dict(chek='citi', street=street, settlement=settlement, address=address, altnames=altnames)
+    if cheek == 's':
+        # region = 46; subtotal_region = 00400100000;
+        like_street = '{}____'.format(region + subtotal_region)
+        like_doma = '{}______'.format(region + subtotal_region)
+        street = Street.query.filter(Street.code.like(like_street)).order_by(Street.name).all()
+        doma = Doma.query.filter(Doma.code.like(like_doma)).order_by(Doma.name).all()
+        notquery = '{}'.format(region + subtotal_region)
+        address = Kladr.query.filter(Kladr.code.like(notquery)).all()
+        altnames = altnames_kladr(notquery)
+        template_context = dict(chek='settlement', street=street, doma=doma, address=address, altnames=altnames)
+
+    return render_template('kladr/kladr_distrikt.html', **template_context)
